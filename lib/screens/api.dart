edb6e91d50e398b159ecf66d40c1563f57955894
souza.leandror@gmail.com <souza.leandror@gmail.com>
2019-07-11 18:04:48 -0300

    Update 10%

--- lib/screens/api.dart
+++ lib/screens/api.dart
@@ -1,1 +1,10 @@
 const API_KEY = "AIzaSyBXxvwxgjYrPb6WOCPixWuInWVCwZvxn30";
+const search = "";
+const _search = "";
+const _nextToken = "";
+const api1 =
+    "https://www.googleapis.com/youtube/v3/search?part=snippet&q=$search&type=video&key=$API_KEY&maxResults=10";
+const api2 =
+    "https://www.googleapis.com/youtube/v3/search?part=snippet&q=$_search&type=video&key=$API_KEY&maxResults=10&pageToken=$_nextToken";
+const api3 =
+    "http://suggestqueries.google.com/complete/search?hl=en&ds=yt&client=youtube&hjson=t&cp=1&q=$search&format=5&alt=json";
